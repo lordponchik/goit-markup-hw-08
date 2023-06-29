dc1836d8ffa066cb42be17e454884cfc69de49ae
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-06-29 09:43:19 +0200

    change name openOrderBtn

--- js/render.js
+++ js/render.js
@@ -1,8 +1,8 @@
 import modal from "./modal.js";
 import { renderOrder } from "./modalOrder.js";
 
-const openModalBtn = document.querySelector("[data-modal-open]");
+const openOrderBtn = document.querySelector("[data-order-open]");
 
-const modalOrder = new modal(openModalBtn, renderOrder());
+const modalOrder = new modal(openOrderBtn, renderOrder());
 
 modalOrder.openAddEvent();
