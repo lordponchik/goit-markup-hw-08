a05c1fa5c3ec0a37b8838b00b8b97f3da831d959
Denys Strashniuk <strashnyuk.denis@icloud.com>
2023-06-29 09:33:43 +0200

    change modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,30 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    document.body.classList.toggle('modal-open');
-    refs.modal.classList.toggle('is-hidden');
+export default class modal {
+  constructor(btnOpen, data) {
+    this.data = data;
+    this.openModalBtn = btnOpen;
+    this.backdrop = document.querySelector(".backdrop");
+    this.modal = document.querySelector(".modal");
+    this.closeModalBtn;
   }
-})();
+  toggleModal() {
+    document.body.classList.toggle("modal-open");
+    this.backdrop.classList.toggle("is-hidden");
+  }
+  render() {
+    this.modal.insertAdjacentHTML("beforeend", this.data);
+  }
+  openAddEvent() {
+    this.openModalBtn.addEventListener("click", () => {
+      this.render();
+      this.toggleModal();
+      this.closeAddEvent();
+    });
+  }
+  closeAddEvent() {
+    this.closeModalBtn = document.querySelector("[data-modal-close]");
+    this.closeModalBtn.addEventListener("click", () => {
+      this.toggleModal();
+      this.modal.innerHTML = "";
+    });
+  }
+}
